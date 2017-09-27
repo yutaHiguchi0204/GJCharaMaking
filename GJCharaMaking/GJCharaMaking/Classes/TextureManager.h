f0626a8ab7540ca39974e89713d65bce28fb2e2a
TSITCL\s162149 <yuta.programmer2016@gmail.com>
2017-09-27 15:51:12 +0900

    2D用追加

---
+++ GJCharaMaking/GJCharaMaking/Classes/TextureManager.h
@@ -0,0 +1,59 @@
+/* =====================================================================
+//! @param		「TextureManager」クラスのヘッダファイル
+//! @create		樋口　裕太
+//! @date		17/06/05
+===================================================================== */
+#pragma once
+
+// ヘッダファイルのインクルード
+#include "SingletonDirector.h"
+
+#include <CommonStates.h>
+#include <d3d11_1.h>
+#include <SimpleMath.h>
+#include <SpriteBatch.h>
+#include "Constant.h"
+
+// クラスの定義
+class TextureManager : public SingletonDirector<TextureManager>
+{
+public:
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_context;
+	ID3D11BlendState* m_blendState;
+
+private:
+	friend class SingletonDirector<TextureManager>;
+
+	// コンストラクタ
+	TextureManager() { m_blendState = NULL; };
+
+public:
+
+	// 初期化処理
+	void Initializer(ID3D11Device* device, ID3D11DeviceContext* context);
+
+	// テクスチャのロード
+	void Load(wchar_t* fileName, ID3D11ShaderResourceView** texture);
+
+	// ブレンドステートの設定
+	void SetBlendState();
+
+	// テクスチャの描画
+	void Draw(
+		ID3D11ShaderResourceView* texture,													// テクスチャハンドル
+		DirectX::SimpleMath::Vector2 pos,													// 表示座標
+		float angle = 0.0f,																	// 回転角
+		DirectX::SimpleMath::Vector2 textureSize = DirectX::SimpleMath::Vector2(0, 0),		// テクスチャサイズ
+		DirectX::SpriteEffects flip = DirectX::SpriteEffects::SpriteEffects_None			// 反転処理
+	);
+	void Draw(
+		ID3D11ShaderResourceView* texture,													// テクスチャハンドル
+		DirectX::SimpleMath::Vector2 pos,													// 表示座標
+		DirectX::SimpleMath::Vector2 rectPos,												// 切り取る矩形の開始位置
+		DirectX::SimpleMath::Vector2 rectSize,												// 切り取る矩形サイズ
+		float angle = 0.0f,																	// 回転角
+		DirectX::SimpleMath::Vector2 textureSize = DirectX::SimpleMath::Vector2(0, 0),		// テクスチャサイズ
+		DirectX::SpriteEffects flip = DirectX::SpriteEffects::SpriteEffects_None			// 反転処理
+	);
+};
